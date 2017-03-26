7a2c6a561640cf44c38dbcde5286bfe2dea06ff1
John Snevily <snev7821@vandals.uidaho.edu>
2017-03-26 16:19:46 -0700

    Added Parent Pickup Class
    Added a parent pickup class,
    a capsule pickup child to affect player
    and a test script on the player to use pickups

---
+++ Assets/Scripts/PlayerPowerup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowerup : Pickup
+{
+    public string powerupType; //What does it do. Probably a better way to do this
+    public float lengthOfPowerup; //how many seconds does it last?
+
+    public override void ActivatePowerup()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        player.GetComponentInChildren<ActivatePlayerPowerup>().PowerUp(lengthOfPowerup, powerupType);
+    }
+	
+}
