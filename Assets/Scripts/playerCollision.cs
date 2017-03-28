cf82dba1156e53f97a4075b79129f494720ce221
John Snevily <snev7821@vandals.uidaho.edu>
2017-03-28 01:23:53 -0700

    Grass, Fine tuning level01
    Made some slight adjustments after playtesting level01.
    Added adhoc invincibility method to playercontroller. Super fun, and
    rediculous, but would quickly ruin the game if overused.

--- Assets/Scripts/playerCollision.cs
+++ Assets/Scripts/playerCollision.cs
@@ -5,16 +5,29 @@ public class playerCollision : MonoBehaviour
     public GameObject playerPieces;
     public PlayerMovement movement;
 
+    public bool isInvincible = false;
+
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
-            movement.enabled = false;
-            Instantiate(playerPieces, transform.position, transform.rotation);
-            FindObjectOfType<GameManager>().EndGame();
-            Destroy(gameObject);
+            if (isInvincible)
+            {
+                //movement.enabled = false;
+                Instantiate(playerPieces, transform.position, transform.rotation);
+                //FindObjectOfType<GameManager>().EndGame();
+                //Destroy(gameObject);
+            }
+            else
+            {
+                movement.enabled = false;
+                Instantiate(playerPieces, transform.position, transform.rotation);
+                FindObjectOfType<GameManager>().EndGame();
+                Destroy(gameObject);
+            }
 
         }
     }
+
 
 }
